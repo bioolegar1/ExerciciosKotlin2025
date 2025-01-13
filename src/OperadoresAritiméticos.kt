a290680b32cf0bfef27d3e25d5337c655e6a216b
Vanderson H Santos <bioolegari@gmail.com>
2025-01-13 14:55:44 -0300

    operadores

---
+++ src/OperadoresAritiméticos.kt
@@ -0,0 +1,18 @@
+fun main ( ) {
+
+    var valorA = 10.0
+    var valorB = 4.0
+
+    var soma = valorA.plus(valorB)
+    var subtracao = valorA.minus(valorB)
+    var multiplicacao = valorA.times(valorB)
+    var divisor = valorA.div(valorB)
+    var modulo = valorA.mod(valorB)
+
+    println("Com as variáveis \n valorA = $valorA e valorB = $valorB \n o resultado das operações são:")
+    println("Soma: $soma")
+    println("Subtracao: $subtracao")
+    println("Multiplicacao: $multiplicacao")
+    println("Divisor: $divisor")
+    println("Modulo: $modulo")
+}
