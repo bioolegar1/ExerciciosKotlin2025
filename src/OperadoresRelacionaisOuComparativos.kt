5af0fa906e40a7d4ac8b5f80f137849a5403b2a0
Vanderson H Santos <bioolegari@gmail.com>
2025-01-13 22:11:59 -0300

    Operadores RElacionais ou Comparativos

---
+++ src/OperadoresRelacionaisOuComparativos.kt
@@ -0,0 +1,15 @@
+fun main (){
+
+    // == - ===
+
+    var valorA:String = "Brasília"
+    var valorB:String = "Brasília"
+
+    var comparacao =  (valorA === valorB)
+    var comparacao2 =  valorA.compareTo(valorB)
+
+
+    println("bolean: $comparacao \n" +
+            "A diferença entre $valorA e $valorB é: $comparacao2")
+
+}
