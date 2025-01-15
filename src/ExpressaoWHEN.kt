af96eb1e88163d7101d25847dd3d59f801646875
Vanderson H Santos <bioolegari@gmail.com>
2025-01-15 20:29:15 -0300

    when

---
+++ src/ExpressaoWHEN.kt
@@ -0,0 +1,17 @@
+/**
+ * Expressão when
+ */
+
+fun main () {
+
+    var valorA = 10
+
+    when(valorA) {
+
+        10 -> print("valorA = 10")
+        20 -> print("valorA = 20")
+        else -> print("valorA não é 10 nem 20")
+    }
+
+
+}
